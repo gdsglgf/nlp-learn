f3a525ab2d15efe040dde7201d1508e6a2923c42
gdsglgf <wzguofulgz@126.com>
2017-06-26 12:49:30 +0800

    add join sql

--- sql/sql-history.sql
+++ sql/sql-history.sql
@@ -4,3 +4,16 @@ SELECT * FROM `t_html` WHERE title like '%儿童%'
 SELECT * FROM `t_link` WHERE link_text = '体育运动';
 
 SELECT * FROM `t_html` where title like '%公司%' limit 0, 10;
+
+
+-- rela_type_id 可以简化两个join的问题
+
+select * from t_relation t1
+join (select entity_type_id, name from t_entity where entity_type_id = 1) as t2 on t1.entity_id1 = t2.entity_type_id
+join (select entity_type_id, name from t_entity where entity_type_id = 7) as t3 on t1.entity_id2 = t3.entity_type_id
+limit 10;
+
+select * from t_relation t1
+join (select entity_type_id, name from t_entity where entity_type_id = 1) as t2 on t1.entity_id1 = t2.entity_type_id
+limit 10;
+
